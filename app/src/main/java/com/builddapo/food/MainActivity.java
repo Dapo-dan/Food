697782251563b27a970641b78d5460cd8a042deb
Oladapo <oladapo.o@yahoo.com>
2021-06-24 17:32:13 +0100

    24 june

--- app/src/main/java/com/builddapo/food/MainActivity.java
+++ app/src/main/java/com/builddapo/food/MainActivity.java
@@ -2,13 +2,63 @@ package com.builddapo.food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView  textView1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //to remove title bar and action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
+        textView1 = (TextView)findViewById(R.id.app_name);
+        textView2 = (TextView)findViewById(R.id.developer);
+
+        textView1.animate().alpha(0f).setDuration(0);
+        textView2.animate().alpha(0f).setDuration(0);
+
+        textView1.animate().alpha(1f).setDuration(1000).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView2.animate().alpha(1f).setDuration(800);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
     }
 }
